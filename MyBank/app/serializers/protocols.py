3c08a8a4a767f092b08a1b95ddad8f2f53601563
yyklimenko <yyklimenko@gmail.com>
2023-05-07 17:17:05 +0600

    fixed: Serializer protocol has been separated to SerializerNonInitProtocol and SerializerInitProtocol

--- MyBank/app/serializers/protocols.py
+++ MyBank/app/serializers/protocols.py
@@ -1,11 +1,16 @@
 from typing import Protocol, Any
 
 
-class SerializerProtocol(Protocol):
-    data: dict[str, Any]
-    validated_data: dict[str, Any]
+class SerializerNonInitProtocol(Protocol):
 
     def __call__(self, *args, **kwargs): ...
 
+
+class SerializerInitProtocol(SerializerNonInitProtocol, Protocol):
+    data: dict[str, Any]
+    validated_data: dict[str, Any]
+
     def is_valid(self, *args, **kwargs): ...
 
+
+SerializerProtocol = SerializerInitProtocol | SerializerNonInitProtocol
